9198fc4266f0b3cf2aa9347722e857245abe1f6d
Dipyaman Biswas <Dipyaman.Biswas@energyexemplar.com>
2021-06-02 09:08:21 +0530

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <table>
+      <tr>
+        <th>City</th>
+        <th>AQI</th>
+        <th>Timestamp</th>
+      </tr>
+      <tr v-for="item in gridData" :key="item.city">
+        <td>{{ item.city }}</td>
+        <td>{{ item.data.aqi.toFixed(2) }}</td>
+        <td>{{ item.data.timestamp }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component
+export default class Home extends Vue {
+  private items = [];
+
+  get gridData(): any {
+    const gridData = this.items.map((x) => ({
+      city: x.city,
+      data: x.metrics[x.metrics.length - 1],
+    }));
+    return gridData;
+  }
+
+  created() {
+    const connection = new WebSocket('ws://city-ws.herokuapp.com');
+    connection.onmessage = (event: any) => {
+      // eslint-disable-next-line no-debugger
+      const newData = JSON.parse(event.data).map((x) => ({
+        ...x,
+        timestamp: Date.now(),
+      }));
+
+      newData.forEach((item): void => {
+        const cityAdded = this.items.find((x: any) => x.city === item.city);
+        if (cityAdded) {
+          cityAdded.metrics.push({ aqi: item.aqi, timestamp: item.timestamp });
+        } else {
+          this.items.push({
+            city: item.city,
+            metrics: [{ aqi: item.aqi, timestamp: item.timestamp }],
+          });
+        }
+      });
+    };
+  }
+}
 </script>
